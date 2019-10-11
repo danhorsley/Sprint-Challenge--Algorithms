29f6cb31488023535858b19dd9b475fd552679ec
danielhorsley <danielhorsley@users.noreply.github.com>
2019-10-11 21:10:17 +0100

    all working

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -93,35 +93,35 @@ class SortingRobot:
         return self._light == "ON"
 
     def sort(self):
-        print(self._list,self._light,self._position)
+        #print(self._list,self._light,self._position)
         if self._item is None and self._light=="ON" and self._position==0:
                 return "done"
         self.set_light_on()
 
-        if self.compare_item() is None:
-            self.swap_item()
-        if self.can_move_right():
-            if self.compare_item()==-1:
+        while self.can_move_right(): #new line
+            if self.compare_item() is None:
                 self.swap_item()
-                self.set_light_off()
-                self.move_right()
-                self.sort()
-            else:
-                self.move_right()
-                self.sort()
-        elif self.compare_item()==1:
+            if self.can_move_right():
+                if self.compare_item()==-1:
+                    self.swap_item()
+                    self.set_light_off()
+                    self.move_right()
+                else:
+                    self.move_right()
+
+        if self.compare_item()==1: #change from elif
             self.swap_item()
             self.set_light_off()
-            while self.can_move_left():
+        self.set_light_on()
+        while self.can_move_left(): #unindented
                 self.move_left()
-            if self._item is None and self._light=="ON":
+                if self.compare_item()==1:
+                    self.swap_item()
+                    self.set_light_off()
+        if self._item is None and self._light=="ON":
                 return "done"
-            else:
-                self.sort()
         else:
-            while self.can_move_left():
-                self.move_left()
-            self.sort()
+                self.sort()
 
 
 if __name__ == "__main__":
